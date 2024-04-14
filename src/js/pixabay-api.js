642bf4f419d673ebb8c3a9ff28e0667e8346da72
Yaroslav Matiash <nice.matyash99@gmail.com>
2024-04-14 15:40:52 +0300

    refactoring the code and adding new features

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,17 +1,26 @@
-const API_KEY = '43258004-167cb59a45a78bad01bc517c7';
-const BASE_URL = 'https://pixabay.com/api/?';
+import axios from 'axios';
 
-export function fetchCards(searchQuery) {
-  const SEARCH_PARAMS = new URLSearchParams({
+const API_KEY = '43258004-167cb59a45a78bad01bc517c7';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export async function fetchCards(searchQuery, page) {
+  const params = {
     key: API_KEY,
     q: searchQuery,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
-  });
+    per_page: 15,
+    page: page,
+  };
 
-  return fetch(`${BASE_URL}${SEARCH_PARAMS}`).then(res => {
-    if (!res.ok) throw new Error('Failed to fetch');
-    return res.json();
-  });
+  try {
+    const response = await axios.get(BASE_URL, { params });
+    if (response.status !== 200) {
+      throw new Error('Failed to fetch');
+    }
+    return response.data;
+  } catch (error) {
+    throw new Error('Failed to fetch');
+  }
 }
--- src/main.js
+++ src/main.js
@@ -1,20 +1,32 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { fetchCards } from './js/pixabay-api';
 import { renderCards } from './js/render-functions';
 
 const searchForm = document.querySelector('.search-form');
 const searchInput = document.querySelector('.search-input');
 const cardsList = document.querySelector('.card-list');
+const loadMoreBtn = document.querySelector('.load-more-btn');
 const loader = document.querySelector('.loader');
 
+let currentPage = 1;
+let searchQuery = '';
+
 searchForm.addEventListener('submit', onSearchFormSubmit);
+loadMoreBtn.addEventListener('click', onLoadMore);
 
-function onSearchFormSubmit(e) {
+const lightbox = new SimpleLightbox('.card-item a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+async function onSearchFormSubmit(e) {
   e.preventDefault();
-
-  const searchQuery = searchInput.value.trim();
+  searchQuery = searchInput.value.trim();
   if (!searchQuery) {
     iziToast.error({
       title: 'Error',
@@ -24,27 +36,77 @@ function onSearchFormSubmit(e) {
     return;
   }
 
+  currentPage = 1;
   cardsList.innerHTML = '';
   toggleLoader();
+  try {
+    const data = await fetchCards(searchQuery, currentPage);
+    renderCards(cardsList, data);
+    showLoadMoreBtn(data.hits.length);
+    searchInput.value = '';
+  } catch (error) {
+    handleError(error);
+  } finally {
+    toggleLoader();
+  }
+}
 
-  fetchCards(searchQuery)
-    .then(data => {
-      renderCards(cardsList, data);
-      searchInput.value = '';
-    })
-    .catch(error => {
-      iziToast.error({
-        title: 'Error',
-        message: 'Failed to fetch images. Please try again later.',
-        position: 'topRight',
-      });
-      console.error('Fetch error:', error);
-    })
-    .finally(() => {
-      toggleLoader();
-    });
+async function onLoadMore(e) {
+  e.preventDefault();
+  currentPage++;
+  toggleLoader();
+  try {
+    const data = await fetchCards(searchQuery, currentPage);
+    renderCards(cardsList, data);
+    lightbox.refresh();
+    showLoadMoreBtn(data.hits.length);
+    handleLoadMore(data);
+    smoothScrollToNextGroup();
+  } catch (error) {
+    handleError(error);
+  } finally {
+    toggleLoader();
+  }
+}
+
+function smoothScrollToNextGroup() {
+  const cardHeight = cardsList
+    .querySelector('.card-item')
+    .getBoundingClientRect().height;
+  window.scrollBy({ top: cardHeight * 2, behavior: 'smooth' });
 }
 
 function toggleLoader() {
   loader.classList.toggle('is-hidden');
+}
+
+function showLoadMoreBtn(numOfResults) {
+  if (numOfResults > 0) {
+    loadMoreBtn.classList.remove('is-hidden');
+  } else {
+    loadMoreBtn.classList.add('is-hidden');
+  }
+}
+
+function handleError(error) {
+  iziToast.error({
+    title: 'Error',
+    message: 'Failed to fetch images. Please try again later.',
+    position: 'topRight',
+  });
+  console.error('Fetch error:', error);
+}
+
+function handleLoadMore(data) {
+  const totalHits = data.totalHits || 0;
+  const currentImagesCount = cardsList.querySelectorAll('.card-item').length;
+
+  if (totalHits <= currentImagesCount) {
+    loadMoreBtn.classList.add('is-hidden');
+    iziToast.info({
+      title: 'End of Results',
+      message: "We're sorry, but you've reached the end of search results.",
+      position: 'topRight',
+    });
+  }
 }
